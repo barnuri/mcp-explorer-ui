b39b147d94c9b6138c172416e3bedbb52c0f8116
Bar Nuri <barnu@checkpoint.com>
2025-03-23 13:36:55 +0200

    route fixes

--- app/routes.ts
+++ app/routes.ts
@@ -1,9 +1,9 @@
-import { type RouteConfig, index } from '@react-router/dev/routes';
+import { type RouteConfig } from '@react-router/dev/routes';
 
 export const baseURL = process.env.BASE_PATH ? process.env.BASE_PATH : '/';
 
 export default [
-    index('routes/explore.tsx'),
-    { path: 'mcp-server/:id', file: 'routes/mcp-server.tsx' },
-    { path: 'url-viewer', file: 'routes/url-viewer.tsx' },
+    { path: `${baseURL}`, file: 'routes/explore.tsx' },
+    { path: `${baseURL}mcp-server/:id`, file: 'routes/mcp-server.tsx' },
+    { path: `${baseURL}url-viewer`, file: 'routes/url-viewer.tsx' },
 ] satisfies RouteConfig;
